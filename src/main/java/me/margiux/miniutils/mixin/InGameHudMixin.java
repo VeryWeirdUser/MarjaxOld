e1e66b3f2af35c0c007c1118e11f27f48197e8b0
margiux <hkajusgt@gmail.com>
2023-12-26 21:37:46 +0200

    New mixin

---
+++ src/main/java/me/margiux/miniutils/mixin/InGameHudMixin.java
@@ -0,0 +1,16 @@
+package me.margiux.miniutils.mixin;
+
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+    @Inject(at = @At(value = "HEAD"), method = "render")
+    public void render(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        //TODO RENDER2D EVENT
+    }
+}
